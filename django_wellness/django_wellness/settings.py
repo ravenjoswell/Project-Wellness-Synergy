9d5ba9eb6979e5c514087b7185c3c4e86e5be0d4
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-04 17:05:04 -0700

    Render

--- django_wellness/django_wellness/settings.py
+++ django_wellness/django_wellness/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(=ejf7vyrce=1*5aqg=xwe+c%q50$i^7m1qwa*osx5#8t(p93'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
