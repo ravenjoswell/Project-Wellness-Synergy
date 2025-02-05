04c89645b17e85ea97832ce299b4bf8b10df966d
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-04 17:16:49 -0700

    Render

--- react_wellness/src/utilities.jsx
+++ react_wellness/src/utilities.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 //   baseURL: "http://127.0.0.1:8000/api/",
 // });
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://wellness-synergy-django.onrender.com/api/"
 });
 
 
