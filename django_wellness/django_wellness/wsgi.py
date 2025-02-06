5dbd10d8f5ad2f228d272c53e8b1926bc7e064bd
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-05 17:35:04 -0700

    Render

--- django_wellness/django_wellness/wsgi.py
+++ django_wellness/django_wellness/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'api.deployment_setting' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'django_wellness.settings'
+settings_module = 'django_wellness.deployment_setting' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'django_wellness.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
