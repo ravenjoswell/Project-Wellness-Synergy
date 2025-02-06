568cc9b292b79dc4429d28c36f94dfec1fcda90e
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-05 17:40:13 -0700

    Render

--- django_wellness/manage.py
+++ django_wellness/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'django_wellness.deployment_setting' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'django_wellness.settings'
+    settings_module = 'django_wellness.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'django_wellness.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
