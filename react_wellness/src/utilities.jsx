74a1f5356186f95d01e65a5d957dee86bea41f60
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-08 12:40:06 -0700

    Update

--- react_wellness/src/utilities.jsx
+++ react_wellness/src/utilities.jsx
@@ -20,7 +20,7 @@ import axios from "axios";
 // });
 
 const api = axios.create({
-  baseURL: 'https://wellness-synergy-django.onrender.com/api/',
+  baseURL: 'https://wellness-synergy-react.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
