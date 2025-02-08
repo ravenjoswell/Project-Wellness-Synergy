270177eaf79784fffdc18dbcf16ac9fc34c91fa2
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-08 12:30:37 -0700

    Update

--- django_wellness/django_wellness/settings.py
+++ django_wellness/django_wellness/settings.py
@@ -35,7 +35,8 @@ DEBUG = False
 # ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://wellness-synergy-django.onrender.com",  # Your React app's URL in production
+    "https://wellness-synergy-django.onrender.com",
+    "https://wellness-synergy-react.onrender.com",
 ]
 
 # ALLOWED_HOSTS = []
