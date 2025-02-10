acd47b583b3ed79ce886094eef6c51d8ad3bca0c
ravenjoswell <rjc.panlaqui@gmail.com>
2025-02-09 20:44:56 -0700

    Docker and Render

--- django_wellness/django_wellness/settings.py
+++ django_wellness/django_wellness/settings.py
@@ -30,29 +30,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-# # CORS_ALLOWED_ORIGINS = [
-# #     'http://localhost:5173'
-# # ]
-# # CORS_ALLOWED_ORIGINS = [
-# #     'http://localhost:3000'
-# # ]
 CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ALLOWED_ORIGINS = [
-#     "https://wellness-synergy-react.onrender.com",
-# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
-ALLOWED_HOSTS = ['wellness-synergy-django.onrender.com']
-
-
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1']
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
-
 
 # Application definition
 
@@ -169,7 +154,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_wellness/django_wellness/wsgi.py
+++ django_wellness/django_wellness/wsgi.py
@@ -15,9 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_wellness.settings')
 
 application = get_wsgi_application()
 
-# In wsgi.py, after get_wsgi_application()
-from django.contrib.auth import get_user_model
-User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(username='admin', email='admin@ws.com', password='ws-proj2025')
